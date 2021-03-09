45af1f6fcece6b4d7b4958ff8ceb72aa5601f684
David Radvan <david.radvan@gmail.com>
2021-03-08 22:12:39 -0800

    added timer2.js

---
+++ timer2.js
@@ -0,0 +1,23 @@
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+
+  if (key === "1" || key === "2" || key === "3" || key === "4" || key === "5" || key === "6" || key === "7" || key === "8" || key === "9") {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, Number(key) * 1000);
+  }
+
+});
